d474f870cd4af1a9ff05198664de3e5b2d223e1d
tasneemkhaled <khaledtasneem402@gmail.com>
2025-12-14 22:18:19 +0200

    finish get responsive font size method

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -10,14 +10,43 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Title Text', style: TextStyle(fontSize: 20)),
+          Text(
+            'Title Text',
+            style: TextStyle(
+              fontSize: getResponsiveFontSize(context, fontSize: 20),
+            ),
+          ),
           SizedBox(height: 10),
           Text(
             'This is body text . It should be responsive to the screen size.',
-            style: TextStyle(fontSize: 16),
+            style: TextStyle(
+              fontSize: getResponsiveFontSize(context, fontSize: 16),
+            ),
           ),
         ],
       ),
     );
+  }
+}
+
+// scale factor
+// responsive font size
+//(min,max)size
+double getResponsiveFontSize(context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = scaleFactor * fontSize;
+  double lowerLimit = fontSize * 0.8;
+  double upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < 600) {
+    return width / 400;
+  } else if (width < 900) {
+    return width / 700;
+  } else {
+    return width / 1000;
   }
 }
